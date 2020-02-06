6ecee40acec6e0ae453d62f60071e497860e7332
Hien Nguyen <d.hien.ng@samsung.com>
2020-02-06 15:14:08 +0700

    change max scale of sticker

--- app/src/main/java/com/hmman/photodecoration/model/Layer.kt
+++ app/src/main/java/com/hmman/photodecoration/model/Layer.kt
@@ -52,7 +52,7 @@ open class Layer(
     internal interface Limits {
         companion object {
             const val MIN_SCALE = 0.1f
-            const val MAX_SCALE = 4.0f
+            const val MAX_SCALE = 1.5f
             const val INITIAL_ENTITY_SCALE = 0.4f
         }
     }
